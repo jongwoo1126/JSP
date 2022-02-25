d126be428f9dc7400b3993869fc31d2a4a916927
unknown <lucky4527@gmail.com>
2022-02-25 16:58:07 +0900

    2022/02/25 수업예제

---
+++ JspStudy/src/main/java/bean/StudentsBean.java
@@ -0,0 +1,50 @@
+package bean;
+
+public class StudentsBean {
+	
+	private String sid;
+	private String name;
+	private int gender;
+	private String hp;
+	private int grade;
+	private String rdate;
+	public String getSid() {
+		return sid;
+	}
+	public void setSid(String sid) {
+		this.sid = sid;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getGender() {
+		return gender;
+	}
+	public void setGender(int gender) {
+		this.gender = gender;
+	}
+	public String getHp() {
+		return hp;
+	}
+	public void setHp(String hp) {
+		this.hp = hp;
+	}
+	public int getGrade() {
+		return grade;
+	}
+	public void setGrade(int grade) {
+		this.grade = grade;
+	}
+	public String getRdate() {
+		return rdate;
+	}
+	public void setRdate(String rdate) {
+		this.rdate = rdate;
+	}
+	
+	
+	
+}
